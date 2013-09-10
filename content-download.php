6b33ee998d4925712e427b4d982a9ae6fa8cd92c
佚站互联 <zan@yeahzan.com>
2013-09-11 03:10:33 +0800

    2.0.1 text change
    some text change

--- content-download.php
+++ content-download.php
@@ -20,11 +20,11 @@
 			<p>有任何问题都可以在<a href="http://www.yeahzan.com/zanblog/message">留言板</a>处提出，感谢您的意见与建议。</p>
 			<p class="alert alert-danger">在下载主题之后请务必看一下使用说明和改动日志，常见问题都可以在那里得到答案。<p>
 			<p>
-				<a class="btn btn-large" onclick="_gaq.push(['_trackEvent', 'Jumbotron actions', 'Download', 'Download 3.0.0']);" href="http://pan.baidu.com/share/link?shareid=1888597972&uk=3291201722" target="_blank">下载 V2.0.1</a>
+				<a class="btn btn-large" onclick="_gaq.push(['_trackEvent', 'Jumbotron actions', 'Download', 'Download 3.0.0']);" href="http://pan.baidu.com/share/link?shareid=2471894483&uk=3291201722" target="_blank">下载 V2.0.1</a>
 			
-				<a class="btn btn-large" onclick="_gaq.push(['_trackEvent', 'Jumbotron actions', 'Download', 'Download 3.0.0']);" href="http://www.yeahzan.com/zanblog/archives/131.html">使用说明</a>
+				<a class="btn btn-large" onclick="_gaq.push(['_trackEvent', 'Jumbotron actions', 'Download', 'Download 3.0.0']);" href="http://www.yeahzan.com/zanblog/archives/457.html">使用说明</a>
 
-				<a class="btn btn-large" onclick="_gaq.push(['_trackEvent', 'Jumbotron actions', 'Download', 'Download 3.0.0']);" href="http://www.yeahzan.com/zanblog/archives/435.html">改动日志</a>
+				<a class="btn btn-large" onclick="_gaq.push(['_trackEvent', 'Jumbotron actions', 'Download', 'Download 3.0.0']);" href="http://www.yeahzan.com/zanblog/archives/510.html">改动日志</a>
 			</p>
 		</div>
 	</div>
